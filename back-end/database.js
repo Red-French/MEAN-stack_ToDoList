99f8747b50e968ff7ac4282776c2ac3a2c4ca073
Red-French <red@redfrench.net>
2016-04-12 20:29:37 -0500

    add color/style to console log error msg

--- back-end/database.js
+++ back-end/database.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var colors = require('colors');
 
 mongoose.connect('mongodb://localhost:mean-todo', (err) => {  // to connect to MongoDB, use the Mongoose connect method
   if (err) {
-    throw err;
+    // throw err;
+    console.log(colors.red.bold.underline('Failed to connect to MongoDB. (Make sure MongoDB is running.)'));
   } else {
     console.log('Successfully connected to MongoDB!');
   }
